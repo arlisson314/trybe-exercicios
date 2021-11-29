5a490560ec0a9813d80abb694268098ebd0a33ce
Arlisson N. Costa <arlisson314@gmail.com>
2021-11-29 15:32:44 -0500

    exercicio for

--- 01_Fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/array.js
+++ 01_Fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/array.js
@@ -1,21 +1,22 @@
 
 // Exercício 1
 
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let menuServices = menu[1];
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
 
-// console.log(menuServices);
+console.log(menuServices);
 
 // Exercício 2
 
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let indexOfPortfolio = menu.indexOf('Portfólio');
+ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+ let indexOfPortfolio = menu.indexOf('Portfólio');
 
-// console.log(indexOfPortfolio);
+ console.log(indexOfPortfolio);
 
 // Exercício 3
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato');
+ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+ menu.push('Contato');
 
-console.log(menu);
+ console.log(menu);
+
