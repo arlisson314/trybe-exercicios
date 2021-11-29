6a5f02e4f761f5cf7fb54cdef8e9104a98df6044
Arlisson N. Costa <arlisson314@gmail.com>
2021-11-28 21:50:02 -0500

    atividade 2

---
+++ 01_Fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-01JavaScript-Primeiros-passos/compadores.js
@@ -0,0 +1,12 @@
+const a = 10;
+const b = 11;
+
+if (a === b) {
+    console.log("'a' é igual 'b'");
+}
+else if (a > b) {
+    console.log("'a' é maior que 'b'");
+}
+else {
+    console.log("'b' é mairo que 'a'");
+}
