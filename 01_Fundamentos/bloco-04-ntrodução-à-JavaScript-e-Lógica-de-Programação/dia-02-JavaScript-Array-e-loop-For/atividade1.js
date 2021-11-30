84a0fbff1fdbc849886aeb76437dcf2b57ea91af
Arlisson N. Costa <arlisson314@gmail.com>
2021-11-29 20:22:54 -0500

    atividade 3

--- 01_Fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/atividade1.js
+++ 01_Fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/atividade1.js
@@ -15,3 +15,13 @@
 //     soma += numbers[cont] 
 // }
 // console.log(soma);
+
+
+// 3: Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for ( let cont = 0; cont < numbers.length; cont += 1) {
+    soma += numbers[cont] 
+}
+console.log(soma / numbers.length);
