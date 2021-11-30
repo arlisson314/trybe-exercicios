5ecb44bc4abfcfa870685799730384348902f607
Arlisson N. Costa <arlisson314@gmail.com>
2021-11-29 20:14:22 -0500

    atividade 2

--- 01_Fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/atividade1.js
+++ 01_Fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/atividade1.js
@@ -1,7 +1,17 @@
 // 1: Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for ( let i = 0; i < numbers.length; i += 1) {
-    console.log( numbers[i]);
-}
+// for ( let i = 0; i < numbers.length; i += 1) {
+//     console.log( numbers[i]);
+// }
+
+
+// 2: Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// for ( let cont = 0; cont < numbers.length; cont += 1) {
+//     soma += numbers[cont] 
+// }
+// console.log(soma);
